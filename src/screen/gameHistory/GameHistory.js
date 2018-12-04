6a7e9a1b2b50db1a8ad476599eccf38d0afe8695
sleepbear <sleepbear669@gmail.com>
2018-12-04 17:20:35 +0900

    아무것도 안 했지만 또 커밋

--- src/screen/gameHistory/GameHistory.js
+++ src/screen/gameHistory/GameHistory.js
@@ -104,9 +104,6 @@ const styles = theme => ({
         width: '100%',
         marginTop: theme.spacing.unit * 3,
     },
-    table: {
-        minWidth: 1020,
-    },
     tableWrapper: {
         overflowX: 'auto',
     },
@@ -115,22 +112,7 @@ const styles = theme => ({
 class GameHistory extends React.Component {
     state = {
         order: 'desc',
-        orderBy: 'average',
-        data: [
-            createData('Cupcake', 305, 3.7, 67, 4.3),
-            createData('Donut', 452, 25.0, 51, 4.9),
-            createData('Eclair', 262, 16.0, 24, 6.0),
-            createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-            createData('Gingerbread', 356, 16.0, 49, 3.9),
-            createData('Honeycomb', 408, 3.2, 87, 6.5),
-            createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-            createData('Jelly Bean', 375, 0.0, 94, 0.0),
-            createData('KitKat', 518, 26.0, 65, 7.0),
-            createData('Lollipop', 392, 0.2, 98, 0.0),
-            createData('Marshmallow', 318, 0, 81, 2.0),
-            createData('Nougat', 360, 19.0, 9, 37.0),
-            createData('Oreo', 437, 18.0, 63, 4.0),
-        ]
+        orderBy: 'average'
     };
 
     componentDidMount() {
@@ -156,7 +138,7 @@ class GameHistory extends React.Component {
 
     render() {
         const {classes} = this.props;
-        const {data, order, orderBy} = this.state;
+        const {order, orderBy} = this.state;
         let gameData = [];
         if (this.props.gameResult !== null) {
             gameData = Object.keys(this.props.gameResult)
